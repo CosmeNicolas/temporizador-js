799e36546e56fd5e15f5cfdb0e00f467f13a3f1e
CosmeNicolas <nicomas7590@gmail.com>
2023-11-26 21:24:20 -0300

    agregamos la funcion actualizar tiempo, resetear , iniciar, y pausar

--- js/temportizador.js
+++ js/temportizador.js
@@ -1,2 +1,47 @@
 /* Realizar una web con un temporizador donde el usuario pueda ingresar un tiempo desde donde comenzará a decrementar el contador. Debe contener los botones, iniciar, pausar y reset. 
  */
+
+let tiempo;
+let tiempoDescuento = 0
+
+const iniciar = () =>{
+  //console.log('usamos el boton')
+ let valorTiempo = document.getElementById('valorTiempo').value;
+  if(valorTiempo && valorTiempo > 0){
+   tiempoDescuento = parseInt(valorTiempo);
+   tiempo = setInterval(actualizartiempo,1000)
+   //console.log(tiempo)
+  } else {
+    alert('ingrese un tiempo valido')
+  }
+  
+  }
+  
+const pausar = ()=>{
+  clearInterval(tiempo)
+}
+
+const resetear = () =>{
+  clearInterval(tiempo)
+  tiempoDescuento = 0;
+  document.getElementById('valorTiempo').value = '';
+  document.getElementById('tiempo').innerText = '00:00:00';
+}
+  
+const actualizartiempo = () =>{
+  if(tiempoDescuento){
+    tiempoDescuento--;
+    const horas = Math.floor(tiempoDescuento / 3600);
+    console.log(horas,minutos,segundos)
+        const minutos = Math.floor((tiempoDescuento % 3600) / 60);
+        
+        const segundos = tiempoDescuento % 60;
+        const mostrarTiempo = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+        document.getElementById('tiempo').innerText = mostrarTiempo;
+  } else {
+    clearInterval(tiempo)
+  }
+}
+
+
+
